63cd86749752c72d90d0c74f1f65b4cac3b5e354
Vishwanath Nadikatla <Vishwanath-Nadikatla@TELETRACKING.COM>
2022-11-15 18:21:25 +0530

    Initial commit

---
+++ src/main/java/com/interview/repository/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.interview.repository;
+import org.springframework.data.repository.CrudRepository;
+import com.interview.model.Employee;
+public interface EmployeeRepository extends CrudRepository<Employee, Integer>
+{
+}
